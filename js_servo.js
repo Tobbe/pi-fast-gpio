321f29a3024a7c24cf7bdeeed7e0dfd415c86f4f
Tobbe Lundberg <tobbe@tlundberg.com>
2014-11-25 22:25:48 +0000

    Put the pigpio stuff in its own module

--- js_servo.js
+++ js_servo.js
@@ -1,20 +1,23 @@
-var net = require('net');
-var Put = require('put');
+var PiGPIOjs = require('./pigpio.js');
 
-var SERVO_GPIO_1 = 18;
-var SERVO_GPIO_2 = 23;
+var SERVO_1_GPIO = 18;
+var SERVO_2_GPIO = 23;
 var HOST = '127.0.0.1';
 var PORT = 8888;
 var run = true;
 var pw = 1100;
 var change = 50;
 
-init(function(err, client) {
+var gpio = new PiGPIOjs();
+
+gpio.connect(HOST, PORT, function(err) {
+  if (err) throw err;
+
   setTimeout(function() { run = false; }, 20000);
 
   var servoUpdateInterval = setInterval(function() {
-    setServoPulsewidth(client, SERVO_GPIO_1, pw);
-    setServoPulsewidth(client, SERVO_GPIO_2, pw);
+    gpio.setServoPulsewidth(SERVO_1_GPIO, pw);
+    gpio.setServoPulsewidth(SERVO_2_GPIO, pw);
     console.log('servo pulsewidth ' + pw + ' microseconds.');
     pw += change;
 
@@ -23,84 +26,10 @@ init(function(err, client) {
     }
 
     if (!run) {
-      setServoPulsewidth(client, SERVO_GPIO_1, 0); // servo off
-      setServoPulsewidth(client, SERVO_GPIO_2, 0); // servo off
+      gpio.setServoPulsewidth(SERVO_1_GPIO, 0); // servo off
+      gpio.setServoPulsewidth(SERVO_2_GPIO, 0); // servo off
+      gpio.close();
       clearInterval(servoUpdateInterval);
     }
   }, 500);
 });
-
-function init(cb) {
-  var client = new net.Socket();
-  client.on('data', function(data) {
-    console.log('DATA: ');
-    logDataStream(data);
-  });
-
-  client.connect(PORT, HOST, function() {
-    console.log('CONNECTED TO pigpiod');
-
-    // Disable the Nagle algoritm
-    client.setNoDelay(true);
-
-    cb(null, client);
-  });
-}
-
-/**
- * def set_servo_pulsewidth(self, user_gpio, pulsewidth):
- *     """
- *     Starts (500-2500) or stops (0) servo pulses on the gpio.
- * 
- *     user_gpio:= 0-31.
- *     pulsewidth:= 0 (off),
- *                  500 (most anti-clockwise) - 2500 (most clockwise).
- * 
- *     The selected pulsewidth will continue to be transmitted until
- *     changed by a subsequent call to set_servo_pulsewidth.
- * 
- *     The pulsewidths supported by servos varies and should probably
- *     be determined by experiment. A value of 1500 should always be
- *     safe and represents the mid-point of rotation.
- * 
- *     You can DAMAGE a servo if you command it to move beyond its
- *     limits.
- * 
- *     ...
- *     pi.set_servo_pulsewidth(17, 0)    # off
- *     pi.set_servo_pulsewidth(17, 1000) # safe anti-clockwise
- *     pi.set_servo_pulsewidth(17, 1500) # centre
- *     pi.set_servo_pulsewidth(17, 2000) # safe clockwise
- *     ...
- *     """
- *     return _u2i(_pigpio_command(
- *        self.sl, _PI_CMD_SERVO, user_gpio, int(pulsewidth)))
- */
-function setServoPulsewidth(socketClient, userGpio, pulsewidth) {
-  var cmd = Put()
-            .word32le(8) // _PI_CMD_SERVO
-            .word32le(userGpio)
-            .word32le(pulsewidth)
-            .word32le(0);
-
-  socketClient.write(cmd.buffer());
-}
-
-function logDataStream(data){  
-  // log the binary data stream in rows of 8 bits
-  var print = "";
-  for (var i = 0; i < data.length; i++) {
-    print += " " + data[i].toString(16);
-
-    // apply proper format for bits with value < 16, observed as int tuples
-    if (data[i] < 16) { print += "0"; }
-
-    // insert a line break after every 8th bit
-    if ((i + 1) % 8 === 0) {
-      print += '\n';
-    }
-  }
-
-  // log the stream
-  console.log(print);
-}
---
+++ pigpio.js
@@ -0,0 +1,75 @@
+var net = require('net');
+var Put = require('put');
+
+/** @constructor */
+function PiGPIOjs() {}
+
+/**
+ * Connects to the pigpio daemon.
+ *
+ * This method has to be called before it's possible to do anything with
+ * the GPIO pins.
+ *
+ * The callback will be called when the connection has been established or
+ * if an error occurs.
+ *
+ * @param {string} host - The host to connect to
+ * @param {number} port - The port on the host to connect to
+ * @param {function} cb - Callback function
+ */
+PiGPIOjs.prototype.connect = function(host, port, cb) {
+  this.client = net.connect({host: host, port: port});
+  
+  this.client.on('connect', function() {
+    // Disable the Nagle algoritm
+    this.client.setNoDelay(true);
+
+    cb();
+  }.bind(this));
+
+  this.client.on('error', function(e) {
+    cb(e);
+  });
+};
+
+/**
+ * Half-closes the connection. We might still get some data from the server.
+ */
+PiGPIOjs.prototype.close = function() {
+  this.client.end();
+};
+
+/**
+ * Starts (500-2500) or stops (0) servo pulses on the given gpio pin.
+ * 
+ * The selected pulsewidth will continue to be transmitted until
+ * changed by a subsequent call to set_servo_pulsewidth.
+ * 
+ * The pulsewidths supported by servos varies and should probably
+ * be determined by experiment. A value of 1500 should always be
+ * safe and represents the mid-point of rotation.
+ * 
+ * You can DAMAGE a servo if you command it to move beyond its
+ * limits.
+ * 
+ *     gpio.setServoPulsewidth(17, 0)    # off
+ *     gpio.setServoPulsewidth(17, 1000) # safe anti-clockwise
+ *     gpio.setServoPulsewidth(17, 1500) # centre
+ *     gpio.setServoPulsewidth(17, 2000) # safe clockwise
+ *
+ * @param {number} userGpio The number of the gpio to address. 0-31.
+ * @param {number} pulsewidth The servo pulsewidth to generate
+ *              0 (off),
+ *              500 (most anti-clockwise) - 2500 (most clockwise).
+ */
+PiGPIOjs.prototype.setServoPulsewidth = function(userGpio, pulsewidth) {
+  var cmd = Put()
+            .word32le(8) // _PI_CMD_SERVO
+            .word32le(userGpio)
+            .word32le(pulsewidth)
+            .word32le(0); // Not used
+
+  this.client.write(cmd.buffer());
+};
+
+module.exports = PiGPIOjs;
